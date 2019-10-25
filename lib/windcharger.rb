d5d1b072b0d91cd69ea8b7a07adbfb64df94bfa0
Andrew Marshall <andrew@johnandrewmarshall.com>
2019-10-24 20:22:57 -0400

    Enable frozen string literals

--- lib/windcharger.rb
+++ lib/windcharger.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 require 'windcharger/version'
 
 require 'windcharger/attributes'
--- lib/windcharger/version.rb
+++ lib/windcharger/version.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 module Windcharger
   VERSION = '0.3.0'
 end
